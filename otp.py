7f37064185d0b898118e4cdd377877c3ba4b4b11
Lubos Kozmon <lubosh91@gmail.com>
2018-04-28 10:07:53 +0200

    Add support for file input

--- otp.py
+++ otp.py
@@ -1,5 +1,4 @@
 import sys
-import getpass
 import argparse
 
 
@@ -27,10 +26,10 @@ def validate(txt, key, charset):
     key_set_diff = set(key).difference(charset)
 
     if len(txt_set_diff) > 0:
-        raise Exception(f"txt contains illegal characters {txt_set_diff}")
+        raise Exception("txt contains illegal characters %s" % txt_set_diff)
 
     if len(key_set_diff) > 0:
-        raise Exception(f"key contains illegal characters {key_set_diff}")
+        raise Exception("key contains illegal characters %s" % key_set_diff)
 
     len_txt = len(txt)
     len_key = len(key)
@@ -41,36 +40,57 @@ def validate(txt, key, charset):
     return True
 
 
+def get_charset(file):
+    with open(file, "r") as charset_file:
+        return "".join(charset_file.read().splitlines())
+
+
+def get_txt(file):
+    if file == "-":
+        return input("Enter text: ")
+    else:
+        with open(file, "r") as txt_file:
+            return "".join(txt_file.read().splitlines())
+
+
+def get_key(file):
+    with open(file, "r") as key_file:
+        return "".join(key_file.read().splitlines())
+
+
 def main():
     parser = argparse.ArgumentParser(description="One-time pad")
-    parser.add_argument("-c", "--charset", help="path to charset file; pick required minimum", required=True)
-    parser.add_argument("-k", "--keyfile", help="path to the key file", required=True)
-    parser.add_argument("-o", "--offset", help="key offset; defaults to 0", type=int, default=0)
+    parser.add_argument("-c", "--charsetfile", required=True,
+                        help="path to charset file; pick required minimum")
+    parser.add_argument("-k", "--keyfile", required=True,
+                        help="path to the key file")
+    parser.add_argument("-o", "--offset", type=int, default=0,
+                        help="key offset; defaults to 0")
 
     mode_group = parser.add_mutually_exclusive_group(required=True)
     mode_group.add_argument("-e", action='store_true', help="encrypt")
-    mode_group.add_argument("-d", action='store_true', help="decrypt")
+    mode_group.add_argument("-d", action='store_true',help="decrypt")
+
+    parser.add_argument("txtfile", metavar='TXTFILE', action='store',
+                        help="file with text to en/decrypt. use single dash '-' to read from stdin")
 
     args = parser.parse_args()
 
-    with open(args.charset, "r") as charset_file:
-        charset = "".join(charset_file.read().splitlines())
+    charset = get_charset(args.charsetfile)
+    txt = get_txt(args.txtfile)
+    key = get_key(args.keyfile)
 
-        with open(args.keyfile, "r") as key_file:
-            txt = getpass.getpass("Enter text:")
+    key = key[args.offset:args.offset + len(txt)]
 
-            key = "".join(key_file.read().splitlines())
-            key = key[args.offset:args.offset + len(txt)]
-
-            if validate(txt=txt, key=key, charset=charset):
-                if args.e:
-                    print("Encrypting '%s' with key '%s'" % (txt, key), file=sys.stderr)
-                    print(encrypt(txt=txt, key=key, charset=charset), file=sys.stdout)
-                elif args.d:
-                    print("Decrypting '%s' with key '%s'" % (txt, key), file=sys.stderr)
-                    print(decrypt(txt=txt, key=key, charset=charset), file=sys.stdout)
-                else:
-                    raise Exception("bad mode")
+    if validate(txt=txt, key=key, charset=charset):
+        if args.e:
+            print("Encrypting '%s' with key '%s'" % (txt, key), file=sys.stderr)
+            print(encrypt(txt=txt, key=key, charset=charset), file=sys.stdout)
+        elif args.d:
+            print("Decrypting '%s' with key '%s'" % (txt, key), file=sys.stderr)
+            print(decrypt(txt=txt, key=key, charset=charset), file=sys.stdout)
+        else:
+            parser.print_help()
 
 
 if __name__ == '__main__':
